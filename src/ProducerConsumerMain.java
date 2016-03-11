d54664333a909e1e02e507ff6665cd0efbb484fd
bhavintandel <bhautandel@yahoo.com.com>
2016-03-11 10:13:07 +0000

    deleted extra lines

--- src/ProducerConsumerMain.java
+++ src/ProducerConsumerMain.java
@@ -8,15 +8,12 @@ public class ProducerConsumerMain {
 		Box box = new Box();
 		Thread p = (new Thread(new Producer(box)));
 		Thread c = (new Thread(new Consumer(box)));
-		Thread p1 = (new Thread(new Producer(box)));
-		Thread c1 = (new Thread(new Consumer(box)));
+		
 	
 		
 		p.start();
 		c.start();
-		p1.start();
-		c1.start();
-	
+		
 		
 		
 	}
